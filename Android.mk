49fa47abf0c74276039156fe3756a0b170836da8
spezi77 <spezi7713@gmail.com>
2015-02-08 11:04:51 +0100

    Specify the correct requestor (toolbox) and clean up a little
    Change-Id: Ie2a2daa1f1b598d3984f275ab0d82ff2263d55ef

--- Android.mk
+++ Android.mk
@@ -1,36 +1,21 @@
 # Root AOSP source makefile
-# su is built here, and 
+# su is built here 
 
-my_path := $(call my-dir)
-
-ifdef SUPERUSER_EMBEDDED
-SUPERUSER_PACKAGE := com.spezi77.toolbox
-else
-ifeq ($(SUPERUSER_PACKAGE),)
-SUPERUSER_PACKAGE := com.thirdparty.superuser
-endif
-include $(my_path)/Superuser/Android.mk
-endif
-
-
-LOCAL_PATH := $(my_path)
+LOCAL_PATH :=  $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
-#LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_C_INCLUDES := external/sqlite/dist
 LOCAL_SRC_FILES := Superuser/jni/su/su.c Superuser/jni/su/daemon.c Superuser/jni/su/activity.c Superuser/jni/su/db.c Superuser/jni/su/utils.c Superuser/jni/su/pts.c ../../sqlite/dist/sqlite3.c
-LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\"
-
-ifdef SUPERUSER_PACKAGE_PREFIX
-  LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"$(SUPERUSER_PACKAGE_PREFIX)\"
-endif
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION
 
 ifdef SUPERUSER_EMBEDDED
-  LOCAL_CFLAGS += -DSUPERUSER_EMBEDDED
+LOCAL_CFLAGS += -DSUPERUSER_EMBEDDED
+LOCAL_CFLAGS += -DREQUESTOR=\"com.spezi77.toolbox\"
+LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"com.spezi77.toolbox.superuser\"
 endif
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
@@ -50,23 +35,3 @@ ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
 # local module name
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
-
-ifdef SUPERUSER_EMBEDDED
-
-# make sure init.superuser.rc is imported from
-# init.rc or similar
-
-#SUPERUSER_RC := $(TARGET_ROOT_OUT)/init.superuser.rc
-#$(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
-#	$(copy-file-to-new-target)
-
-SUPERUSER_MARKER := $(TARGET_OUT_ETC)/.has_su_daemon
-$(SUPERUSER_MARKER): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) touch $@
-
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
-    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SUPERUSER_RC) $(SUPERUSER_MARKER)
-
-endif
