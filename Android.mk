75b775b92fbfd5d85bd24e1d747ca84d14018741
spezi77 <spezi7713@gmail.com>
2015-02-08 02:39:05 +0100

    Point SUPERUSER_PACKAGE to my Toolbox
    Change-Id: Id811391958bd3aab872c66d790d5bd3bb86a50f2

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@
 my_path := $(call my-dir)
 
 ifdef SUPERUSER_EMBEDDED
-SUPERUSER_PACKAGE := com.android.settings
+SUPERUSER_PACKAGE := com.spezi77.toolbox
 else
 ifeq ($(SUPERUSER_PACKAGE),)
 SUPERUSER_PACKAGE := com.thirdparty.superuser
@@ -17,7 +17,8 @@ LOCAL_PATH := $(my_path)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
-LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_C_INCLUDES := external/sqlite/dist
@@ -37,12 +38,14 @@ include $(BUILD_EXECUTABLE)
 
 
 SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
-$(SYMLINKS):
+$(SYMLINKS): $(LOCAL_MODULE)
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 	@echo "Symlink: $@ -> /system/xbin/su"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
 	$(hide) ln -sf ../xbin/su $@
 
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
 # We need this so that the installed files could be picked up based on the
 # local module name
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
@@ -53,9 +56,9 @@ ifdef SUPERUSER_EMBEDDED
 # make sure init.superuser.rc is imported from
 # init.rc or similar
 
-SUPERUSER_RC := $(TARGET_ROOT_OUT)/init.superuser.rc
-$(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
-	$(copy-file-to-new-target)
+#SUPERUSER_RC := $(TARGET_ROOT_OUT)/init.superuser.rc
+#$(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
+#	$(copy-file-to-new-target)
 
 SUPERUSER_MARKER := $(TARGET_OUT_ETC)/.has_su_daemon
 $(SUPERUSER_MARKER): $(LOCAL_INSTALLED_MODULE)
